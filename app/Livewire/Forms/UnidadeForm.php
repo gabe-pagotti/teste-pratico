54806a500f1933d6604ba3e35484e14c11dc66ce
Gabriel Pagotti <gabriel_pagotti@outlook.com>
2024-11-12 04:23:08 +0000

    Adicionando validação de CNPJ

--- app/Livewire/Forms/UnidadeForm.php
+++ app/Livewire/Forms/UnidadeForm.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Forms;
 
 use App\Models\Unidade;
+use Illuminate\Validation\Rule;
 use Livewire\Attributes\Validate;
 use Livewire\Form;
 
@@ -16,11 +17,22 @@ class UnidadeForm extends Form
     #[Validate('required|min:5')]
     public $razao_social = '';
 
-    #[Validate('required|min:5')]
     public $cnpj = '';
 
     #[Validate('required|exists:bandeiras,id')]
     public $bandeira = '';
+
+    public function rules()
+    {
+        return [
+            'cnpj' => [
+                'required',
+                'size:18',
+                'regex:/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/',
+                Rule::unique('unidades')->ignore($this->unidade->cnpj, 'cnpj'),
+            ],
+        ];
+    }
 
     public function setUnidade(Unidade $unidade)
     {
