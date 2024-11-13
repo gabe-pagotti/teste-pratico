df943294ed8646e19cd733c28099124e3c22298e
Gabriel Pagotti <gabriel_pagotti@outlook.com>
2024-11-13 02:17:29 +0000

    Corrigindo o formulário de unidades

--- resources/views/livewire/unidade/form.blade.php
+++ resources/views/livewire/unidade/form.blade.php
@@ -9,21 +9,21 @@
         <!-- Nome Fantasia -->
         <div>
             <x-input-label for="form.nome_fantasia" :value="__('Nome Fantasia')" />
-            <x-text-input wire:model="form.nome_fantasia" id="form.nome_fantasia" class="block mt-1 w-full" type="text" name="form.nome_fantasia" required autofocus value="{{ $form->unidade->nome }}" />
+            <x-text-input wire:model="form.nome_fantasia" id="form.nome_fantasia" class="block mt-1 w-full" type="text" name="form.nome_fantasia" required autofocus value="{{ $form->unidade->nome_fantasia }}" />
             <x-input-error :messages="$errors->get('form.nome_fantasia')" class="mt-2" />
         </div>
 
         <!-- Razão Social -->
         <div>
             <x-input-label for="form.razao_social" :value="__('Razão Social')" />
-            <x-text-input wire:model="form.razao_social" id="form.razao_social" class="block mt-1 w-full" type="text" name="form.razao_social" required autofocus value="{{ $form->unidade->nome }}" />
+            <x-text-input wire:model="form.razao_social" id="form.razao_social" class="block mt-1 w-full" type="text" name="form.razao_social" required autofocus value="{{ $form->unidade->razao_social }}" />
             <x-input-error :messages="$errors->get('form.razao_social')" class="mt-2" />
         </div>
 
         <!-- CNPJ -->
         <div>
             <x-input-label for="form.cnpj" :value="__('CNPJ')" />
-            <x-text-input wire:model="form.cnpj" id="form.cnpj" class="block mt-1 w-full" type="text" name="form.cnpj" required autofocus value="{{ $form->unidade->nome }}" />
+            <x-text-input wire:model="form.cnpj" id="form.cnpj" class="block mt-1 w-full" type="text" name="form.cnpj" required autofocus value="{{ $form->unidade->cnpj }}" />
             <x-input-error :messages="$errors->get('form.cnpj')" class="mt-2" />
         </div>
 
